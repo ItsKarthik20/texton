1944b142ff0c8c8cfdb89f73f1f8b1f11e268202
itsKarthik20 <k.addala126@gmail.com>
2021-09-26 00:30:55 +0530

    fourth commit

--- appname/src/App.js
+++ appname/src/App.js
@@ -1,19 +1,41 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+import UseWindowDimensions from "./UseWindowDimensions";
 
 function App() {
-  return(
+  const [{ user }, dispatch] = useStateValue();
+  const { width } = UseWindowDimensions();
+  const uid =
+    localStorage.getItem("uid") !== undefined
+      ? localStorage.getItem("uid")
+      : null;
 
+  return (
     <div className="app">
-      <div className="app__body">
-        <Sidebar/>
-
-      </div>       
+      {!user && !uid ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
-  
-
 }
 
 export default App;
--- appname/src/firebase.js
+++ appname/src/firebase.js
@@ -1,10 +1,22 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD0vqfyLuilvkDYEPWRWu43BES42cp2F2I",
-    authDomain: "texton-6ab65.firebaseapp.com",
-    projectId: "texton-6ab65",
-    storageBucket: "texton-6ab65.appspot.com",
-    messagingSenderId: "273502679414",
-    appId: "1:273502679414:web:58f94da2f48feb8b9f0a57",
-    measurementId: "G-NBPTCKY14X"
-  };
+  apiKey: "AIzaSyD0vqfyLuilvkDYEPWRWu43BES42cp2F2I",
+  authDomain: "texton-6ab65.firebaseapp.com",
+  projectId: "texton-6ab65",
+  storageBucket: "texton-6ab65.appspot.com",
+  messagingSenderId: "273502679414",
+  appId: "1:273502679414:web:58f94da2f48feb8b9f0a57",
+  measurementId: "G-NBPTCKY14X"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
