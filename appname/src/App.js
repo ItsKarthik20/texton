7e8de344cd206cf5f33c5e136c6632bf10c85e71
itsKarthik20 <k.addala126@gmail.com>
2021-09-25 17:17:01 +0530

    initial commit

---
+++ appname/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Sidebar from "./Sidebar";
+
+function App() {
+  return(
+
+    <div className="app">
+      <div className="app__body">
+        <Sidebar/>
+
+      </div>       
+    </div>
+  );
+  
+
+}
+
+export default App;
---
+++ appname/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD0vqfyLuilvkDYEPWRWu43BES42cp2F2I",
+    authDomain: "texton-6ab65.firebaseapp.com",
+    projectId: "texton-6ab65",
+    storageBucket: "texton-6ab65.appspot.com",
+    messagingSenderId: "273502679414",
+    appId: "1:273502679414:web:58f94da2f48feb8b9f0a57",
+    measurementId: "G-NBPTCKY14X"
+  };
